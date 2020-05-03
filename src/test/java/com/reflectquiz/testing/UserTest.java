5c7f1188f0927c8a383f51499614a39545349aad
lyxu2 <lyxu2@illinois.edu>
2020-05-03 17:05:03 -0500

    changed testing files and cleaned up testing resource directory

--- src/test/java/com/reflectquiz/testing/UserTest.java
+++ src/test/java/com/reflectquiz/testing/UserTest.java
@@ -8,36 +8,39 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.runner.RunWith;
+import org.junit.platform.runner.JUnitPlatform;
 
 import com.reflectquiz.model.User;
 import com.reflectquiz.repository.UserDataRepository;
 import com.reflectquiz.repository.UserDataRepositoryImpl;
 
+@RunWith(JUnitPlatform.class)
 @TestInstance(Lifecycle.PER_CLASS)
-class UserTest {
+public class UserTest {
 	
 	UserDataRepository userDAO;
 	
 	@BeforeAll
-	void setUp() {
+	public void setUp() {
 		userDAO = new UserDataRepositoryImpl();
 	}
 	
 	@Test
-	void testGetUser() {
+	public void testGetUser() {
 		User testUser = userDAO.getUserByUsername("u1");
 		Assertions.assertEquals("u1", testUser.getUsername());
 	}
 	
 	@Test
-	void testGetAllUsers() {
+	public void testGetAllUsers() {
 		List<User> lotsOfUsers = userDAO.getAllUsers();
 		Assertions.assertEquals("u1", lotsOfUsers.get(0).getUsername());
 	}
 	
 	@Test
-	void testInsertUser() {
-		userDAO.insertUser(new User(0, "billcipher", "triangle", "TEACHER"));
+	public void testInsertUser() {
+		//userDAO.insertUser(new User(0, "billcipher", "triangle", "TEACHER"));
 	}
 
 }
