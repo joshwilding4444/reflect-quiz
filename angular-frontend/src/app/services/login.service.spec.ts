9683c69b523bd498ca11aa30bcc40f58935ff35b
joshwilding2011 <joshwilding2011@gmail.com>
2020-05-06 17:09:51 -0500

    Refactored login code. Unit tests pass for login service.

--- angular-frontend/src/app/services/login.service.spec.ts
+++ angular-frontend/src/app/services/login.service.spec.ts
@@ -1,16 +1,24 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { LoginService } from './login.service';
 
 describe('LoginService', () => {
   let service: LoginService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LoginService);
+    TestBed.configureTestingModule({
+       imports: [HttpClientTestingModule],
+       providers: [LoginService]
+    });
+    service = TestBed.get(LoginService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should take a username and password', ()=>{
+       expect(service.getLogin('billcipher', 'triangle')).toBeTruthy();
+  });
+
 });
