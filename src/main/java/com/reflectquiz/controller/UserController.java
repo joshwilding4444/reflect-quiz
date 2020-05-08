b9b07d975b5f72d39c0bc728f9a5e7d0faec812f
joshwilding2011 <joshwilding2011@gmail.com>
2020-05-07 19:57:30 -0500

    Added @CrossOrigin annotation to all Controller classes.

--- src/main/java/com/reflectquiz/controller/UserController.java
+++ src/main/java/com/reflectquiz/controller/UserController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import com.reflectquiz.model.User;
 import com.reflectquiz.repository.UserDataRepositoryImpl;
 import com.reflectquiz.service.UserService;
 
+@CrossOrigin
 @RestController("UserController")
 @RequestMapping(path = "/users")
 public class UserController {
